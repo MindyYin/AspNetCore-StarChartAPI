bca0758849b283333f04e3d281c833ac1f2e2e7a
Mindy Yin <44931995+MindyYin@users.noreply.github.com>
2018-11-10 15:28:20 -0800

    Test3

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -12,10 +12,10 @@ namespace StarChart.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public int? OrbitObject { get; set; }
+        public int? OrbitedObjectId { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
-        public TimeSpan Orbitalperiod { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
 
     }
 }
